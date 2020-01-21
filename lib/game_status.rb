2664509eb080422626a8c08e7ecc2688b2ffd064
Julie  McManus <julieannesays@gmail.com>
2020-01-21 00:01:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,8 +8,10 @@ WIN_COMBINATIONS = [
   top_row_win = [0,1,2],
   midd_row_win = [3,4,5],
   bottom_row_win = [6,7,8]
-  top_to_bottom_win = [0,3,6]
-  top_to_bottom_win = [1,4,7]
-  top_to_bottom_win = [2,5,8]
+  top_to_bottom_win_1 = [0,3,6]
+  top_to_bottom_win_2 = [1,4,7]
+  top_to_bottom_win_3 = [2,5,8]
+  across_win_1 = [0,4,8]
+  across_win_2 = [6,4,2]
 
 ]
