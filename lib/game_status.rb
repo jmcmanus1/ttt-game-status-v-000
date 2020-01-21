89b127e865c08b3fb75513f637c30d8b56b09dd1
Julie  McManus <julieannesays@gmail.com>
2020-01-21 00:00:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,12 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  top_row_win = [0,1,2],
+  midd_row_win = [3,4,5],
+  bottom_row_win = [6,7,8]
+  top_to_bottom_win = [0,3,6]
+  top_to_bottom_win = [1,4,7]
+  top_to_bottom_win = [2,5,8]
+
+]
