d817b1161188f8e4875654a039325de2b5770b03
Julie  McManus <julieannesays@gmail.com>
2020-01-21 00:06:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   top_row_win = [0,1,2],
-  midd_row_win = [3,4,5],
+  middle_row_win = [3,4,5],
   bottom_row_win = [6,7,8],
   top_to_bottom_left = [0,3,6],
   top_to_bottom_middle = [1,4,7],
